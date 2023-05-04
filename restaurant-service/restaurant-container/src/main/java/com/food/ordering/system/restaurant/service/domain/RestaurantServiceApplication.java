4819bf8bc8fb4deba04fc748685754f531afb665
Mayank Jain <f20190141@pilani.bits-pilani.ac.in>
2023-05-05 01:24:34 +0530

    implemented domain-core & added filestructure for other modules

--- restaurant-service/restaurant-container/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantServiceApplication.java
+++ restaurant-service/restaurant-container/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantServiceApplication.java
@@ -1,4 +1,11 @@
 package com.food.ordering.system.restaurant.service.domain;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.food.ordering.system")
 public class RestaurantServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantServiceApplication.class, args);
+    }
 }
---
+++ restaurant-service/restaurant-domain/restaurant-application-service/src/main/java/com/food/ordering/system/restaurant/service/domain/dto/RestaurantApprovalRequest.java
@@ -0,0 +1,12 @@
+package com.food.ordering.system.restaurant.service.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class RestaurantApprovalRequest {
+
+}
