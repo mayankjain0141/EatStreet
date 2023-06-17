d4f5532f58c0d0562ff7a7f9aceb94abde6f1b0c
Mayank Jain <f20190141@pilani.bits-pilani.ac.in>
2023-06-18 02:50:26 +0530

    refactored restaurant-service

--- restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantDomainServiceImpl.java
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/RestaurantDomainServiceImpl.java
@@ -18,9 +18,7 @@ import static com.food.ordering.system.domain.DomainConstants.TIMEZONE;
 public class RestaurantDomainServiceImpl implements RestaurantDomainService {
     @Override
     public OrderApprovalEvent validateOrder(Restaurant restaurant,
-                                            List<String> failureMessages,
-                                            DomainEventPublisher<OrderApprovedEvent> orderApprovedEventDomainEventPublisher,
-                                            DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher) {
+                                            List<String> failureMessages) {
         restaurant.validateOrder(failureMessages);
         log.info("Validating order with id: {}", restaurant.getOrderDetail().getId().getValue());
 
@@ -30,16 +28,14 @@ public class RestaurantDomainServiceImpl implements RestaurantDomainService {
             return new OrderApprovedEvent(restaurant.getOrderApproval(),
                     restaurant.getId(),
                     failureMessages,
-                    ZonedDateTime.now(ZoneId.of(TIMEZONE)),
-                    orderApprovedEventDomainEventPublisher);
+                    ZonedDateTime.now(ZoneId.of(TIMEZONE)));
         } else {
             restaurant.constructOrderApproval(OrderApprovalStatus.REJECTED);
             log.info("Order is rejected for order id: {}", restaurant.getOrderDetail().getId().getValue());
             return new OrderRejectedEvent(restaurant.getOrderApproval(),
                     restaurant.getId(),
                     failureMessages,
-                    ZonedDateTime.now(ZoneId.of(TIMEZONE)),
-                    orderRejectedEventDomainEventPublisher);
+                    ZonedDateTime.now(ZoneId.of(TIMEZONE)));
         }
     }
 }
--- restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/event/OrderRejectedEvent.java
+++ restaurant-service/restaurant-domain/restaurant-domain-core/src/main/java/com/food/ordering/system/restaurant/service/domain/event/OrderRejectedEvent.java
@@ -8,15 +8,9 @@ import java.time.ZonedDateTime;
 import java.util.List;
 
 public class OrderRejectedEvent extends OrderApprovalEvent {
-    private final DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher;
 
-    public OrderRejectedEvent(OrderApproval orderApproval, RestaurantId restaurantId, List<String> failureMessages, ZonedDateTime createdAt, DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher) {
+    public OrderRejectedEvent(OrderApproval orderApproval, RestaurantId restaurantId, List<String> failureMessages, ZonedDateTime createdAt) {
         super(orderApproval, restaurantId, failureMessages, createdAt);
-        this.orderRejectedEventDomainEventPublisher = orderRejectedEventDomainEventPublisher;
     }
 
-    @Override
-    public void fire() {
-        orderRejectedEventDomainEventPublisher.publish(this);
-    }
 }
